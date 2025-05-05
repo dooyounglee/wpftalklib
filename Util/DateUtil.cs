db35c64e430841bff3d19248530ec4452b1c648a
doo <gostbaducking1@nate.com>
2025-05-05 22:38:47 +0900

    feat: util추가(date, image, number)

---
+++ Util/DateUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talkLib.Util
+{
+    public class DateUtil
+    {
+        public static string now(string format)
+        {
+            // format: yyyy-MM-dd HH:mm:dd.fffffff
+            DateTime now = DateTime.Now;
+            return now.ToString(format);
+        }
+    }
+}
---
+++ Util/NumberUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talkLib.Util
+{
+    public class NumberUtil
+    {
+        public static int random(int from, int to)
+        {
+            Random random = new Random();
+            return random.Next(from, to);
+        }
+    }
+}
