b87810c8e9b14fc3f24aea962aa223ef4a5c060b
doo <gostbaducking1@nate.com>
2025-09-11 00:21:03 +0900

    feat: 오늘은 시간, 어제까지는 날짜 표시하는 함수 추가

--- Util/DateUtil.cs
+++ Util/DateUtil.cs
@@ -10,9 +10,27 @@ namespace talkLib.Util
     {
         public static string now(string format)
         {
-            // format: yyyy-MM-dd HH:mm:dd.fffffff
+            // format: yyyy-MM-dd HH:mm:ss.fffffff
             DateTime now = DateTime.Now;
             return now.ToString(format);
+        }
+
+        public static string TimeWhenToday(string yyyyMMddHHmmss)
+        {
+            var yyyyMMdd = now("yyyyMMdd");
+            if (yyyyMMdd == yyyyMMddHHmmss.Substring(0, 8))
+            {
+                return format(yyyyMMddHHmmss, "HH:mm");
+            } else
+            {
+                return format(yyyyMMddHHmmss, "MM-dd");
+            }
+        }
+
+        public static string format(string yyyyMMddHHmmss, string format)
+        {
+            DateTime dt = DateTime.ParseExact(yyyyMMddHHmmss, "yyyyMMddHHmmss", null);
+            return dt.ToString(format);
         }
     }
 }
