55fbbc07f1c17390841b3220009f4921bd789c07
doo <gostbaducking1@nate.com>
2025-09-12 01:43:47 +0900

    fix: ChatState 정리

--- Models/ChatState.cs
+++ Models/ChatState.cs
@@ -8,9 +8,11 @@ namespace OTILib.Models
 {
   public enum ChatState
   {
-    None, Initial, Connect, Disconnect, ConnectFail,
-    StateChange, Invite, Leave,
-    File, Image,
+    None, Initial, Connect, Disconnect, ConnectFail, // -> #0: 로그인, -> #n: 방 접속
+    StateChange, Profile, // #0 -> #0
+    Create, // #n+1 -> #0
+    Chat, File, Image, Invite, Leave, // #n -> #n,#0
+
     ChatReload, ProfileReload,
-  }
+    }
 }
